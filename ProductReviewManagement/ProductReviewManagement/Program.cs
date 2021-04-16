94763886a2b79594025f766de6a9c0a7a7a63209
Neha <nehak6296@gmail.com>
2021-04-16 23:32:36 +0530

    [Neha]Added:created product review class and list of product review and then added 25 default values in list

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProductReviewManagement
 {
@@ -7,6 +8,42 @@ namespace ProductReviewManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management..");
+
+            List<ProductReview> productReviewList = new List<ProductReview>()
+                {
+                new ProductReview(){ProducID=1,UserID=1,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ProducID=2,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProducID=3,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProducID=4,UserID=1,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProducID=5,UserID=1,Rating=2,Review="nice",isLike=true},
+                new ProductReview(){ProducID=6,UserID=1,Rating=1,Review="bad",isLike=true},
+                new ProductReview(){ProducID=8,UserID=1,Rating=1,Review="Good",isLike=false},
+                new ProductReview(){ProducID=8,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProducID=2,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProducID=10,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProducID=11,UserID=1,Rating=3,Review="nice",isLike=true},
+                new ProductReview(){ProducID=10,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProducID=11,UserID=1,Rating=3,Review="Good",isLike=true},
+                new ProductReview(){ProducID=12,UserID=1,Rating=1,Review="Bad",isLike=false},
+                new ProductReview(){ProducID=13,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProducID=13,UserID=1,Rating=5,Review="Nice",isLike=true},
+                new ProductReview(){ProducID=14,UserID=1,Rating=6,Review="Good",isLike=true},
+                new ProductReview(){ProducID=15,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProducID=16,UserID=1,Rating=1,Review="bad",isLike=false},
+                new ProductReview(){ProducID=17,UserID=1,Rating=10,Review="Nice",isLike=true},
+                new ProductReview(){ProducID=18,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProducID=3,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProducID=5,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProducID=6,UserID=1,Rating=3,Review="nice",isLike=true}
+
+            };
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine("ProductID:- " + list.ProducID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+
+
         }
     }
 }
